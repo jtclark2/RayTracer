a17c66ca2baff805f73ba3f0a7fd89c2f988e58f
Trevor Clark <jtclark2@gmail.com>
2022-02-09 10:52:52 -0800

    Feature: Complete 6.1 - Shading surface normals Completed section 6.1 of the tutorial, and reviewed the concepts leading up to section 6, adding a much better explanation, and some really bad, but instructive ascii art.

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ Run the executable, redirecting the output into an image.ppm file:
 > cd bin\Win32\Debug
 > RayTracing.exe > image.ppm
 ```
+To open ppm files, consider using:
+- Gimp
+- [This Online Viewer] (https://www.cs.rhodes.edu/welshc/COMP141_F16/ppmReader.html)
+
+# Style
+https://pages.nist.gov/dioptra/dev-guide/contributing-commit-styleguide.html
--- RayTracing/src/Main.cpp
+++ RayTracing/src/Main.cpp
@@ -4,6 +4,42 @@ Trevor's thoughts:
 kind of think of it as the image, except that it has a position of project,
 rather than being associated with a pixel. main loops through pixel values to
 find the viewport value at each pixel index.
+
+
+	Scene:
+	-------------------------------------------------------
+		sphere							|
+			sphere						|
+		sphere				*			|(origin)
+										|
+				sphere					|
+
+	-------------------------------------------------------
+
+	My ascii art isn't great, so use your imagination:
+		- The Sphere's are populating the scene. Rays will interact with them.
+		- The focal point is the '*'. This is located at the origin, which all rays pass through.
+		- The wall of `|` represent the image & camera. These are two representations of the same 2D window into the scene.
+		  The main difference is that camera is a 2D window living in the 3D vector space of the scene. The image is the
+		  a necessary abstraction for us to view that scene in pixels. It lives exclusively in a 2D pixel space and it's
+		  only connection to the 3D scene is via the camera.
+			- The wall is located `focal_length` from the focal point.
+				- Currently, this is purely along the Z axis, we could translate the camera pretty easily by changing the definition of 
+				ray, which is based on lower_left_corner. To change the orientation would be a little more involved, but still doable. 
+				 *Maybe I'll work that out if I have time at the end*
+			- Camera lives in vector space with dimensions of viewport_height, viewport_width (width is not shown in my 2D ascii art)
+			  We're using coordinates in which increase from left -> right / bottom -> top.
+			  Therefore, lower_left_corner is at (-viewport_width/2, -viewport_height/2, -focal_length)
+			- Image is then found by looping through discrete locations on the camera, which represent the pixels. At each location,
+			we create a ray, and send it out towards the focal point, checking for collisions along the way. If we hit a sphere, then
+			we return the appropriate color.
+				- Don't light rays travel to a camera, not out of it? In real life, yes. However, that would create so many rays, and
+				we're only interested in the ones that hit the camera. So yes, we are doing it backwards, but and it's running the physics
+				in reverse. Rather than asking "Which photons hit the sensor here?". We are asking "Given that this photon hit the sensor,
+				what did it's journey look like?"
+				- I'll add more about appropriate colors later, since it's a function of illumination, reflection, and a bunch of lighting
+				stuff I haven't gotten to yet 
+	Image is a grid of pixels.
 ******************************************************************************/
 
 #include <iostream>
@@ -13,10 +49,10 @@ find the viewport value at each pixel index.
 #include "vec3.h"
 
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+double hit_sphere(const point3& center, double radius, const ray& r) {
 	// Solve for t
 	// dir^2*t^2 + 2*dir*(pt - Center) + dot(pt-center, pt-center)-r^2 = 0
-	// Quadratic formula: t = -b +/- sqrt(b^2 - 4ac) / (2ac)
+	// Quadratic formula: t = ( -b +/- sqrt(b^2 - 4ac) ) / (2ac)
 	// Simplify a bit further...we don't need the solution, just whether or
 	// not there is one. If the value in the sqrt > 0, then the answers are
 	// real. 
@@ -27,46 +63,60 @@ bool hit_sphere(const point3& center, double radius, const ray& r) {
 	auto a = dot(r.direction(), r.direction());
 	auto b = 2.0 * dot(oc, r.direction());
 	auto c = dot(oc, oc) - radius*radius;
-	auto discriminant = b*b - 4*a*c;
+	auto discriminant = b*b - 4*a*c; // the part of the quadratic formula under the sqrt()
+
 	//std::cerr << "origin: " << r.origin() << " center: " << center <<  "ray at z=-1: " << r.<<  disc: " << discriminant << "\n";
-	return (discriminant > 0);
+	//return (discriminant > 0);
+
+	if (discriminant < 0) { // meaning sphere is hit
+		return -1.0;
+	}
+	else {
+		return (-b - sqrt(discriminant)) / (2.0*a); // one solution for quadratic. We're ignoring the other sign...for now.
+	}
 }
 
 color ray_color(const ray& r) {
-	if (hit_sphere(point3(0, 0, -1), 0.5, r)) { // sphere at0,0,-1; radius=0.5, and we pass the ray to check for intersection
-		return color(1, 0, 0); // red sphere
+	auto hit = hit_sphere(point3(0, 0, -1), 0.5, r);
+	if (hit > 0.0) { // sphere at0,0,-1; radius=0.5, and we pass the ray to check for intersection
+		vec3 N = unit_vector(r.at(hit) - vec3(0, 0, -1));
+		return 0.5*color(N.x() + 1, N.y() + 1, N.z() + 1); // (x+1)*.5 shifts -1 -> 1 distribution to 0 -> 1 
+		//return color(1, 0, 0); // red sphere
 	}
 
 	// Create background/horizon (blue to white fade)
 	vec3 unit_direction = unit_vector(r.direction());
 	// ensure 0-1, since direction magnitudes range: -1 to 1
-	auto t = 0.5*(unit_direction.y() + 1.0);
+	hit = 0.5*(unit_direction.y() + 1.0);
 	// Linear Interpolation (LERP) between white(1,1,1), and blue(0.5,0.7,1.0)
-	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+	return (1.0 - hit) * color(1.0, 1.0, 1.0) + hit * color(0.5, 0.7, 1.0);
 }
 
 int main() {
+
+
+
 
 	// Image
 	const auto aspect_ratio = 16.0 / 9.0;
 	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	//const int image_width = 256;
-	//const int image_height = 256;
 
 	// Camera
 	auto viewport_height = 2.0;
 	auto viewport_width = aspect_ratio * viewport_height;
 	auto focal_length = 1.0;
 
-	auto origin = point3(0, 0, 0);
+	auto origin = point3(0, 0, 0); // location of camera (which is also where rays come from)
 	auto horizontal = vec3(viewport_width, 0, 0);
 	auto vertical = vec3(0, viewport_height, 0);
-	auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+	//auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+	auto lower_left_corner = origin - vec3(viewport_width/2, viewport_height/2, focal_length);
 
 	// Render
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
+	// Loop through all pixels in the image
 	for (int j = image_height - 1; j >= 0; --j) {
 		std::cerr << "\rScanlines remaining: " << j  << " of " << image_height << " " << std::flush;
 		for (int i = 0; i < image_width; ++i) {
