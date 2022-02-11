998ddf0346a711e541f105070339a8d8db1773ef
Trevor Clark <jtclark2@gmail.com>
2022-02-10 22:07:52 -0800

    Feature: Finished section 10, Dialectrics (transparent materials) This extends the previous checkin by dealing with total internal reflection, and the applying Schlick's approximation, which deals with variation in reflection, based on the angle of the incoming ray.

--- RayTracing/src/Main.cpp
+++ RayTracing/src/Main.cpp
@@ -134,24 +134,25 @@ int main() {
 	hittable_list world; // all objects that rays can interact with in the scene (visible stuff)
 
 	auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-	auto material_center = make_shared<dielectric>(1.5);
+	auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
 	auto material_left = make_shared<dielectric>(1.5);
+	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 	//auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
 	//auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
-	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
-	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100, material_ground));
-	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5, material_center));
-	world.add(make_shared<sphere>(point3(-1, 0, -1), 0.5, material_left));
-	world.add(make_shared<sphere>(point3(1, 0, -1), 0.5, material_right));
+	world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
+	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
 
 	// Image
 	const auto aspect_ratio = 16.0 / 9.0; // TODO: we seem to have this hardcoded here and in the camera
 	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	const int samples_per_pixel = 5;
-	const int max_depth = 5;
+	const int samples_per_pixel = 10;
+	const int max_depth = 10;
 
 	// Camera
 	camera cam;
