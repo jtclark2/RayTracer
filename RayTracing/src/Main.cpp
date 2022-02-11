cc3f129be1f3b852749e740a793d66a04c633dd3
Trevor Clark <jtclark2@gmail.com>
2022-02-11 12:35:24 -0800

    Feature: Finished tutorial! Created a randomly generating scene.

--- RayTracing/src/Main.cpp
+++ RayTracing/src/Main.cpp
@@ -53,6 +53,8 @@ find the viewport value at each pixel index.
 #include "camera.h"
 #include "material.h"
 
+#include <time.h>
+
 /*
 As a physics major, I'm amazed how well this simple model is working, despite a lot of assumptions, and missing elemnents.
 This author's done a great job so far, so I'm sure we're getting to at least some of these, but here's a few
@@ -128,51 +130,103 @@ color ray_color(const ray& r, const hittable& world, int depth) {
 	return (1.0 - hit) * color(1.0, 1.0, 1.0) + hit * color(0.5, 0.7, 1.0);
 }
 
+hittable_list random_scene() {
+	hittable_list world;
+
+	auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+
+	// Add a bunch of smaller random spheres
+	for (int a = -11; a < 11; a++) { // position in x + rand
+		for (int b = -11; b < 11; b++) { // position in z + rand
+			auto choose_mat = random_double();
+			point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+
+			if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+				shared_ptr<material> sphere_material;
+
+				if (choose_mat < 0.8) {
+					// diffuse / non-reflective
+					auto albedo = color::random() * color::random(); // TODO: why squared??? Maybe just to lower the ave values a bit?
+					sphere_material = make_shared<lambertian>(albedo);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material)); // TODO: consider randomizing the radiuses as well
+				}
+				else if (choose_mat < 0.95) { // TODO: more intuitive to use the prob of this category, rather than this minus .8 from prev
+					// metal / reflective
+					auto albedo = color::random(0.5, 1);
+					auto fuzz = random_double(0, 0.5);
+					sphere_material = make_shared<metal>(albedo, fuzz);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				}
+				else {
+					// dielectric / glass
+					sphere_material = make_shared<dielectric>(1.52); // 1.52 = index of refraction of glass
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				}
+			}
+		}
+	}
+
+	// A larger show piece for each material
+	auto material1 = make_shared<dielectric>(1.5);
+	world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+	auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+	world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+	auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+	world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+	return world;
+}
+
 int main() {
-
+	
 	///////////////// World /////////////////
-	auto R = cos(pi / 4);
-	hittable_list world; // all objects that rays can interact with in the scene (visible stuff)
+	auto world = random_scene();
+	//auto R = cos(pi / 4);
+	//hittable_list world; // all objects that rays can interact with in the scene (visible stuff)
 
-	//auto material_left   = make_shared<lambertian>(color(1, 0, 0));
-	//auto material_right = make_shared<lambertian>(color(0, 0, 1));
-	//auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-	//auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
-	auto material_left = make_shared<dielectric>(1.5);
-	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
-	auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-	auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+	////auto material_left   = make_shared<lambertian>(color(1, 0, 0));
+	////auto material_right = make_shared<lambertian>(color(0, 0, 1));
+	////auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
+	////auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+	//auto material_left = make_shared<dielectric>(1.5);
+	//auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
+	//auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+	//auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
 
-	//world.add(make_shared<sphere>(point3(-R, 0, -1), R, material_left)); // R to the left, 1 unit away from camera (with rad=R, that should fill the whole screen vertically)
-	//world.add(make_shared<sphere>(point3(R, 0, -1), R, material_right));
-	world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
-	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
-	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
-	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
-	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
+	////world.add(make_shared<sphere>(point3(-R, 0, -1), R, material_left)); // R to the left, 1 unit away from camera (with rad=R, that should fill the whole screen vertically)
+	////world.add(make_shared<sphere>(point3(R, 0, -1), R, material_right));
+	//world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+	//world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+	//world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+	//world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
+	//world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
 	///////////////// Image /////////////////
-	const auto aspect_ratio = 16.0 / 9.0;
-	const int image_width = 200;
+	const auto aspect_ratio = 3.0 / 2.0; //16.0 / 9.0;
+	const int image_width = 1200;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	const int samples_per_pixel = 100;
-	const int max_depth = 10;
+	const int samples_per_pixel = 1;
+	const int max_depth = 50;
 
 	///////////////// Camera /////////////////
-	point3 lookfrom(3, 3, 2);
-	point3 lookat(0, 0, -1);
+	point3 lookfrom(13, 2, 3);
+	point3 lookat(0, 0, 0);
 	vec3 vup = vec3(0, 1, 0); // (0,1,0) is world up
 	double fov_deg = 20;
-	auto dist_to_focus = (lookfrom - lookat).length(); // Move inside the function?
-	auto aperture = 2.0; // 1 would be perfect focus?
+	auto dist_to_focus = 10; // (lookfrom - lookat).length(); // Move inside the function?
+	auto aperture = 0.1; // 1 would be perfect focus?
 	camera cam(lookfrom, lookat, vup, fov_deg, aspect_ratio, aperture, dist_to_focus);
 
 	///////////////// Render /////////////////
+	clock_t tStart = clock();
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 	// Loop through all pixels in the image
 	for (int j = image_height - 1; j >= 0; --j) {
-		std::cerr << "\rScanlines remaining: " << j  << " of " << image_height << " " << std::flush;
+		std::cerr << "\r (" << (double)(clock() - tStart) / CLOCKS_PER_SEC << ") Scanlines remaining: " << j  << " of " << image_height << " " << std::flush;
 		for (int i = 0; i < image_width; ++i) {
 			color pixel_color(0, 0, 0);
 			for (int s = 0; s < samples_per_pixel; ++s) {
@@ -201,8 +255,7 @@ int main() {
 			//write_color(std::cout, pixel_color, samples_per_pixel_perfect_square);
 		}
 	}
-
-	std::cerr << "\nDone.\n";
+	std::cerr << "\nRender Completed in: \n" << (double)(clock() - tStart) / CLOCKS_PER_SEC << "seconds.";
 }
 
 // Ctrl+shft+B to compile solution
